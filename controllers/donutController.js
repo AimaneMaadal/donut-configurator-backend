9a90058215c03fcec19714b0960e2c2610707177
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-11-30 14:14:58 +0100

    feat: create controller for update donut function

--- controllers/donutController.js
+++ controllers/donutController.js
@@ -1,44 +1,58 @@
 const Donut = require("../models/Donut");
 
 const getAllDonuts = async (req, res) => {
-    try {
-        const donuts = await Donut.find();
-        res.status(200).json(donuts);
-    } catch (error) {
-        res.status(404).json({
-            message: error.message
-        });
-    }
+  try {
+    const donuts = await Donut.find();
+    res.status(200).json(donuts);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 const createDonut = async (req, res) => {
-    donut = new Donut([
-        req.body.dough,
-        req.body.glaze,
-        req.body.sprinkles,
-        req.body.company
-    ]);
-    donut.save(function (err, donut) {
-        if (err) {
-            res.status
-        }
-        res.status(201).send(donut);
-    });
+  donut = new Donut([
+    req.body.dough,
+    req.body.glaze,
+    req.body.sprinkles,
+    req.body.company,
+  ]);
+  donut.save(function (err, donut) {
+    if (err) {
+      res.status;
+    }
+    res.status(201).send(donut);
+  });
 };
 
 const deleteDonut = async (req, res) => {
-    try {
-        const donut = await Donut.findByIdAndDelete(req.params.id);
-        res.status(200).json(donut);
-    } catch (error) {   
-        res.status(404).json({
-            message: error.message
-        });
-    }
+  try {
+    const donut = await Donut.findByIdAndDelete(req.params.id);
+    res.status(200).json(donut);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateDonut = async (req, res) => {
+  try {
+    const donut = await Donut.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(donut);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 module.exports = {
-    getAllDonuts,
-    createDonut,
-    deleteDonut
+  getAllDonuts,
+  createDonut,
+  deleteDonut,
+  updateDonut,
 };
