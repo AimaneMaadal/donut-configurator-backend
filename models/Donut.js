7d1496f433f2a9742d5b368929d1edf159018af1
AimaneMaadal <aimanemaadal@gmail.com>
2022-11-30 12:48:49 +0100

    refactor: adjust donut model

--- models/Donut.js
+++ models/Donut.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const donutscheme = new Schema({
+const donutSchema = new Schema({
   dough: {
     type: Number,
-    required: true,
   },
   glaze: {
     type: Number,
-    required: true,
   },
   sprinkles: {
     type: Number,
-    required: true,
   },
   company: {
     type: String,
-    required: true,
   },
 });
 
-const Donut = model("Donuts", donutscheme);
+const Donut = model("Donuts", donutSchema);
 module.exports = Donut;
