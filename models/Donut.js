4403a6b3bb1ca2c86a1594263005819879f81f4e
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-11-30 13:41:06 +0100

    feat: make donut fields required

--- models/Donut.js
+++ models/Donut.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
+const Schema = mongoose.Schema;
 
 const donutSchema = new Schema({
   dough: {
     type: Number,
+    required: true,
   },
   glaze: {
     type: Number,
+    required: true,
   },
   sprinkles: {
     type: Number,
+    required: true,
   },
   company: {
     type: String,
+    required: true,
   },
 });
 
-const Donut = model("Donuts", donutSchema);
+const Donut = mongoose.model("donuts", donutSchema);
+
 module.exports = Donut;
