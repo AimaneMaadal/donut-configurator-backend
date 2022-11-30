5ad0e560415c57a08d2a9803f62943b7dc3aca89
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-11-30 14:20:09 +0100

    feat: create route to search donut by id

--- routes/donuts.js
+++ routes/donuts.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const donutController = require("../controllers/donutController");
 
 router.get("/", donutController.getAllDonuts);
+router.get("/:id", donutController.getDonutById);
+
 router.post("/", donutController.createDonut);
+
 router.delete("/:id", donutController.deleteDonut);
+
 router.put("/:id", donutController.updateDonut);
 
 module.exports = router;
