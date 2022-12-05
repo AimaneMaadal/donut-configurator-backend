ecb45ac398f40f44cdf4bd4867c266cc1c0ac464
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-05 17:23:16 +0100

    feat: enable cors for all routes in app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var donutsRouter = require("./routes/donuts");
@@ -21,6 +22,7 @@ async function main() {
 }
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
