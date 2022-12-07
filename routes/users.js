13809011ff60c61d192d47393f6e3c1be246c16b
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-07 16:09:23 +0100

    feat: create login route

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const authorization = require("../controllers/auth");
 
 router.post("/signup", authorization.signup);
+router.post("/login", authorization.login);
 
 module.exports = router;
