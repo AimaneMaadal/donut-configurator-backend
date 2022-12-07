44b95739d048ca46a4e3fd3720f582c0a310c0e1
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-07 16:10:00 +0100

    feat: add checkAuth function to get all donuts

--- routes/donuts.js
+++ routes/donuts.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const donutController = require("../controllers/donutController");
+const checkAuth = require("../middleware/check-auth");
 
-router.get("/", donutController.getAllDonuts);
+router.get("/", checkAuth, donutController.getAllDonuts);
 router.get("/:id", donutController.getDonutById);
 
 router.post("/", donutController.createDonut);
