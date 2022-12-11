1d725540f59edfe2c182947dd0769fc19824f5b6
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-12-11 12:10:27 +0100

    test: temporarily delete check auth function from get all donuts route

--- routes/donuts.js
+++ routes/donuts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const donutController = require("../controllers/donutController");
 const checkAuth = require("../middleware/check-auth");
 
-router.get("/", checkAuth, donutController.getAllDonuts);
+router.get("/", donutController.getAllDonuts);
 router.get("/:id", donutController.getDonutById);
 
 router.post("/", donutController.createDonut);
