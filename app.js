ebb306dbe179451d395a7c587d67432d2938e771
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-12-23 11:59:50 +0100

    fix: allow all sites to send requests

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ async function main() {
 var app = express();
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
